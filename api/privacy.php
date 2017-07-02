c8f64b0f33d04a6c3d2c56b577a28314c7f0f792
Anuj Jaha <er.anujjaha@gmail.com>
2017-07-02 15:17:27 +0530

    Misc - Updated message key

--- api/privacy.php
+++ api/privacy.php
@@ -3,7 +3,7 @@
 	global $db;
 
 	$policy = $db->single('SELECT data_value FROM settings where data_key = "policy"');
-    $ret 	= array('status' => 'success', 'message' => 'Terms & Condition', 'data' => $policy);
+    $ret 	= array('status' => 'success', 'message' => 'Private Policy', 'data' => $policy);
     echo json_encode($ret);
     die();
 ?>
