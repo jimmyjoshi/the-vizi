529eee7821a7c81e37bb9a05ac32d57dc2fb587a
Anuj Jaha <er.anujjaha@gmail.com>
2017-07-02 17:13:28 +0530

    Policy updated - From JSON To HTML

--- api/terms.php
+++ api/terms.php
@@ -3,7 +3,16 @@
 	global $db;
 	
 	$terms  = $db->single('SELECT data_value FROM settings where data_key = "terms"' );
-    $ret 	= array('status' => 'success', 'message' => 'Terms & Condition', 'data' => $terms);
+    /*$ret 	= array('status' => 'success', 'message' => 'Terms & Condition', 'data' => $terms);
     echo json_encode($ret);
-    die();
+    die();*/
 ?>
+
+<html>
+	<head>
+		<title>Privacy Policy</title>
+	</head>
+	<body>
+		<?php echo $terms;?>
+	</body>	
+</html>
