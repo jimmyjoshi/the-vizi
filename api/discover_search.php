0ef35534a8058b245713b717552f99dd23c0edcb
Anuj Jaha <er.anujjaha@gmail.com>
2017-09-03 12:51:34 +0530

    Discover search - trending based on pin counts:

--- api/discover_search.php
+++ api/discover_search.php
@@ -15,8 +15,8 @@
     if (!isset($_REQUEST['user_id']) || $_REQUEST['user_id'] == '') {
         $ret['message'] = 'User id can not be blank!';
     }
-
     if ($ret['message'] == '') {
+
         /*$ids = $db->query(' SELECT following_id FROM follow WHERE follower_id IN (SELECT following_id FROM follow WHERE follower_id = '.$_REQUEST['user_id'].' ) ');
         $tmps = array();
         if (count($ids) > 0) {
@@ -25,7 +25,12 @@
             }
         }*/
         if (!isset($_REQUEST['name']))
-    	   $users = $db->query('SELECT id, user_name, image, lat, lon, address FROM users WHERE id != ' . $_REQUEST['user_id'] . ' ORDER BY rand() LIMIT 10 ');
+        {
+
+            $users = $db->query('SELECT  (select count(id) from pins where user_id = users.id) as pincount, users.id, users.user_name, users.image, users.lat, users.lon, users.address FROM users
+                    LEFT JOIN pins on pins.user_id = users.id
+                 WHERE users.id != ' . $_REQUEST['user_id'] . ' group by users.id ORDER BY pincount desc  LIMIT 6');
+        }
         else {
             $users = $db->query('SELECT id, user_name, image, lat, lon, address FROM users WHERE user_name LIKE "%'.$_REQUEST['name'].'%" AND id != ' . $_REQUEST['user_id']);
         }
