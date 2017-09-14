fd1ebd7f8f322cdcf8f0b86adf31929a259e33a7
Anuj Jaha <er.anujjaha@gmail.com>
2017-09-14 23:16:05 +0530

    fix bug

--- api/pins_from_city.php
+++ api/pins_from_city.php
@@ -16,18 +16,19 @@
 
         if (!isset($_REQUEST['city_name']) || $_REQUEST['city_name'] == '') 
         {
-            $all_pins = $db->query('SELECT * FROM pins WHERE category_id = '.$_REQUEST['category_id'] . ' AND user_id = ' . $_REQUEST['user_id']);
+           $all_pins = $db->query('SELECT * FROM pins WHERE category_id = '.$_REQUEST['category_id'] . ' AND user_id = ' . $_REQUEST['user_id']);
 
             $pins = array();
             if (count($all_pins) > 0) {
-                foreach ($all_pins as $p) {
+                foreach ($all_pins as $p) 
+                {
                     $u = $db->query('SELECT user_name, image FROM users WHERE id = ' . $p['user_id']);
                     $img = $db->single('SELECT image FROM media WHERE pin_id = ' . $p['id'] . ' LIMIT 1');
                     $img = $img != '' ? $img : DEFAULT_VIZI_IMAGE;
                     $pins[] = array('pin_id' => $p['id'], 'user' => $u[0]['user_name'], 'image' => $img, 'title' => $p['title'], 'lat' => $p['lat'], 'lon' => $p['lon'], 'address' => $p['address']);
                 }
-            $ret['status'] = 'success';
-            $ret['message'] = 'Pin found!';
+                $ret['status'] = 'success';
+                $ret['message'] = 'Pin found!';
         }
         else
         {
