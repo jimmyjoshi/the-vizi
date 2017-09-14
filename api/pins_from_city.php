9fa8743584e51b7702e50b3ae0639f747c0d6309
Anuj Jaha <er.anujjaha@gmail.com>
2017-09-14 23:28:04 +0530

    Distance based on CITY

--- api/pins_from_city.php
+++ api/pins_from_city.php
@@ -14,8 +14,18 @@
 
     if ($ret['message'] == '') {
 
-        if (!isset($_REQUEST['city_name']) || $_REQUEST['city_name'] == '') 
+        if(isset($_REQUEST['city_name']) || $_REQUEST['city_name'] != '') 
         {
+            $city = $_REQUEST['city_name'];
+ 
+            #Find latitude and longitude
+             
+            $url = "http://maps.googleapis.com/maps/api/geocode/json?address=$city";
+            $json_data = file_get_contents($url);
+            $result = json_decode($json_data, TRUE);
+            $latitude = $result['results'][0]['geometry']['location']['lat'];
+            $longitude = $result['results'][0]['geometry']['location']['lng'];
+
            $all_pins = $db->query('SELECT * FROM pins WHERE category_id = '.$_REQUEST['category_id'] . ' AND user_id = ' . $_REQUEST['user_id']);
 
             $pins = array();
@@ -24,10 +34,20 @@
                 foreach ($all_pins as $p) 
                 {
                     $u = $db->query('SELECT user_name, image FROM users WHERE id = ' . $p['user_id']);
+
+                    $disatance = (distance($p['lat'], $p['lon'], $latitude, $longitude)) ? distance( $p['lat'], $p['lon'], $latitude, $longitude) : -1;
+
                     $img = $db->single('SELECT image FROM media WHERE pin_id = ' . $p['id'] . ' LIMIT 1');
                     $img = $img != '' ? $img : DEFAULT_VIZI_IMAGE;
-                    $pins[] = array('pin_id' => $p['id'], 'user' => $u[0]['user_name'], 'image' => $img, 'title' => $p['title'], 'lat' => $p['lat'], 'lon' => $p['lon'], 'address' => $p['address']);
+                    $pins[] = array('pin_id' => $p['id'], 'user' => $u[0]['user_name'], 'image' => $img, 'title' => $p['title'], 'lat' => $p['lat'], 'lon' => $p['lon'], 'address' => $p['address'], 'distance' => $disatance);
                 }
+
+                $result = array();
+                foreach ($pins as $key => $row)
+                {
+                    $result[$key] = $row['distance'];
+                }
+                array_multisort($result, SORT_ASC, $pins);
                 
             }
             
