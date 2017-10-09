32f6a140f508d50abc486ad8f5b9146b6a87cba8
Anuj Jaha <er.anujjaha@gmail.com>
2017-10-09 22:27:22 +0530

    Added Category

--- api/add_user_category.php
+++ api/add_user_category.php
@@ -12,9 +12,11 @@
     	$ret['message'] = 'Title can not be blank!';
     }
 
-    if ($ret['message'] == '') {
-    	$_POST['image'] = '';
-        if (isset($_FILES) && isset($_FILES['image']) && count($_FILES['image']) > 0) {
+    if ($ret['message'] == '') 
+    {
+    	$userImage = '';
+        if (isset($_FILES) && isset($_FILES['image']) && count($_FILES['image']) > 0) 
+        {
             $file = pathinfo($_FILES['image']['name']);
             $name = $file['filename'];
             $ext = $file['extension'];
@@ -22,10 +24,22 @@
             $image = $name . '-' . $rand . '.' . $ext;
             $upload_to = PUBLIC_PATH . 'categories/' . $image;
             move_uploaded_file($_FILES['image']['tmp_name'], $upload_to);
-            $_POST['image'] = PUBLIC_URL . 'categories/' . $image;
+            $userImage = $_POST['image'] = PUBLIC_URL . 'categories/' . $image;
         }
-		$insert = $db->query("INSERT INTO categories(name, user_id, image, is_private) VALUES(:name, :user_id, :image, :private)", $_POST);
-		$new_cat_id = $db->lastInsertId();
+
+        $private = $_POST['private'] ? $_POST['private'] : 0;
+        $sql = 'INSERT INTO  categories(name, user_id, image, is_private) VALUES
+                (
+                    "'.$_POST['name'].'",
+                    "'.$_POST['user_id'].'",
+                    "'.$userImage.'",
+                    "'.$private.'"
+                )';
+        
+        //$insert = $db->query("INSERT INTO categories(name, user_id, image, is_private) VALUES(:name, :user_id, :image, :private)", $_POST);
+        
+        $insert = $db->query($sql);
+        $new_cat_id = $db->lastInsertId();
         $ret['message'] = 'Category created successfully';
         $ret['status'] = 'success';
         $ret['data'] = array('id' => $new_cat_id, 'name' => $_REQUEST['name'], 'image' => $_POST['image']);
