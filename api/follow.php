dc84b0a34d5cf7acc609c64289435f0de80927af
Anuj Jaha <er.anujjaha@gmail.com>
2018-04-16 22:39:30 +0530

    notification Login updated - Remove depreciated functions

--- api/follow.php
+++ api/follow.php
@@ -25,7 +25,7 @@
 
         $sql = 'DELETE FROM notifications WHERE type LIKE "FOLLOW" AND obj_id IN( SELECT id FROM follow 
                 WHERE follower_id = '. $userId .' AND following_id = '. $followerId .' )';
-                
+
 
         $db->query($sql);
 
@@ -41,19 +41,39 @@
             $ret['status'] = 'success';
         }
         else {
-            $private = $db->single('SELECT is_private FROM users WHERE id = ' . $_POST['follow_id']);
+
+
+            $singleQuery = 'SELECT is_private FROM users WHERE id = ' . $_POST['follower_id'];
+
+            $private = $db->single($singleQuery);
     
-            $db->query('INSERT INTO follow (follower_id, following_id, requested) VALUES(:user_id, :following_id, :requested)', array('user_id' => $_REQUEST['user_id'], 'following_id' => $_REQUEST['follow_id'], "requested" => $private));
+            $db->query('INSERT INTO follow (follower_id, following_id, requested) VALUES(:user_id, :following_id, :requested)', array('user_id' => $_REQUEST['user_id'], 'following_id' => $_REQUEST['follower_id'], "requested" => $private));
+
+
             $obj_id = $db->lastInsertId();
-            $db->query('INSERT INTO notifications (user_id, type, obj_id) VALUES(:uid, :type, :obj_id) ', array('uid' => $_REQUEST['user_id'], 'type' => 'FOLLOW', 'obj_id' => $obj_id));
+
+            $usrId = $_REQUEST['user_id'];
+
+
+            $nsql = 'INSERT INTO notifications (user_id, type, obj_id)  VALUES (
+                        "'.$usrId.'",
+                        "follow",
+                        "'.$obj_id.'"
+                    )'; 
+
+            $db->query($nsql);
+
+
             $ret['message'] = $private ? 'Follow request sent!' : 'You are now following!';
             $ret['status'] = 'success';
 
             //To the user to whome current user is now following
-            $token = can_send($_REQUEST['follow_id']);
+            $token = can_send($_REQUEST['follower_id']);
             if ($token) {
+
                 require 'push.php';
-                $name = $db->single('SELECT user_name FROM users WHERE id = ' . $_REQUEST['user_id']);
+                $usrId = $_REQUEST['user_id'];
+                $name = $db->single('SELECT user_name FROM users WHERE id = ' . $usrId);
                 $msg_payload = array (
                     'mtitle'        => 'Vizi',
                     'mdesc'         => $private ? $name . ' has sent you follow request' : $name . ' is now following you',
@@ -62,8 +82,14 @@
                 PushNotifications::iOS($msg_payload, $token);
             }
 
+            $usrId      = $_REQUEST['user_id'];
+            $followerId = $_REQUEST['follower_id'];
+                
+            $userIdSql = 'SELECT device_id FROM users WHERE id IN ( SELECT follower_id FROM follow 
+                        WHERE  following_id = '. $usrId .' AND follower_id != '. $followerId .' ) ';
+
             //To all those users who are following user_id
-            $user_ids = $db->query('SELECT device_id FROM users WHERE id IN (SELECT follower_id FROM follow WHERE following_id = '.$_REQUEST['user_id'].' AND follower_id != '.$_REQUEST['follow_id'].' ) AND notification LIKE "on" ');
+            $user_ids = $db->query($userIdSql);
 
             if (count($user_ids) > 0) 
             {
@@ -71,8 +97,12 @@
                 
                 foreach ($user_ids as $tkn) 
                 {
-                    $following_to = $db->single('SELECT user_name FROM users WHERE id = ' . $_REQUEST['follow_id']);
-                    $followed_by = $db->single('SELECT user_name FROM users WHERE id = ' . $_REQUEST['user_id']);
+                    $followingSQL = 'SELECT user_name FROM users WHERE id = ' . $_REQUEST['follower_id'];
+                    $following_to = $db->single($followingSQL);
+
+                    $followedBySQL = 'SELECT user_name FROM users WHERE id = ' . $_REQUEST['user_id'];
+                    $followed_by = $db->single($followedBySQL);
+
                     $msg_payload = array (
                         'mtitle' => 'Vizi',
                         'mdesc' => $followed_by . ' is now following to ' . $following_to,
