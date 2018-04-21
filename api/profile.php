aa6253291f2c7cbc1a41ef1d6584a9644ddd2a89
Anuj Jaha <er.anujjaha@gmail.com>
2018-04-21 11:19:02 +0530

    FIxed follow button - txt

--- api/profile.php
+++ api/profile.php
@@ -8,9 +8,13 @@
     	$ret['message'] = 'User id can not be blank!';
     }
 
-    if ($ret['message'] == '') {
-    	$user = $db->query('SELECT * FROM users WHERE id = :id', array('id' => $_REQUEST['user_id']));
-    	if (count($user) > 0) {
+    if ($ret['message'] == '') 
+    {
+        $buttonTxt = "Follow";
+    	$user      = $db->query('SELECT * FROM users WHERE id = :id', array('id' => $_REQUEST['user_id']));
+
+    	if (count($user) > 0) 
+        {
     		$user = $user[0];
     		unset($user['reset'], $user['created_at'], $user['status'], $user['role'], $user['password'], $user['visibility'], $user['device_id']);
 
@@ -19,7 +23,8 @@
             $pins = $db->single('SELECT count(id) AS pins FROM pins WHERE user_id = ' . $_REQUEST['user_id']);
             //$cats = $db->query('SELECT id, name, image, user_id FROM categories WHERE id IN (SELECT category_id FROM pins WHERE user_id = ' . $_REQUEST['user_id'] . ')');
             //$cats = $db->query('SELECT id, name, image, user_id FROM categories WHERE user_id = ' . $_REQUEST['user_id'] . '  ');
-            if (isset($_REQUEST['city']) && $_REQUEST['city'] != '') {
+            if (isset($_REQUEST['city']) && $_REQUEST['city'] != '') 
+            {
                 if($_REQUEST['current_user_id'] != $_REQUEST['user_id'])                
                 {
                     $cats = $db->query('SELECT id, name, image, user_id, is_private FROM categories WHERE id IN (SELECT category_id FROM pins WHERE address LIKE "'.$_REQUEST['city'].'" AND is_private = 0 AND user_id = '.$_REQUEST['user_id'].' )');
@@ -29,7 +34,8 @@
                     $cats = $db->query('SELECT id, name, image, user_id, is_private FROM categories WHERE id IN (SELECT category_id FROM pins WHERE address LIKE "'.$_REQUEST['city'].'" AND user_id = '.$_REQUEST['user_id'].' )');    
                 }
             }
-            else {
+            else 
+            {
                 if($_REQUEST['current_user_id'] != $_REQUEST['user_id'])                
                 {
                     $cats = $db->query('SELECT id, name, image, user_id, is_private FROM categories WHERE is_private = 0 AND user_id IN (SELECT id FROM users WHERE role LIKE "admin" OR id = '.$_REQUEST['user_id'].' )');
@@ -42,13 +48,34 @@
 
             $current_id = $_REQUEST['current_user_id'];
             $is_following = "0";
-            if ($current_id != '') {
-                $ex_following = $db->single('SELECT id FROM follow WHERE follower_id =  ' . $current_id . ' AND following_id = ' . $_REQUEST['user_id']);
+            if ($current_id != '') 
+            {
+                $ex_following = $db->query('SELECT * FROM follow WHERE follower_id =  ' . $current_id . ' AND following_id = ' . $_REQUEST['user_id']);
+
+                if(isset($ex_following))
+                {
+                    $ex_following = $ex_following[0];
+                }
+
+                $userProfile  = $db->single('SELECT * FROM users WHERE id =  ' . $current_id );
+
                 if ($ex_following != '')
+                {
                     $is_following = "1";
+                    $buttonTxt = "Following";
+                }
+
+                if($is_following == "1")
+                {
+                   if($ex_following['requested'] == 0 )
+                   {
+                        $buttonTxt = "Requested";
+                   }
+                }
             }
 
-            if (count($cats) > 0) {
+            if (count($cats) > 0) 
+            {
                 foreach ($cats as $k => $c) {
                     if ($current_id == $_REQUEST['user_id'])
                         $cats[$k]['show_delete'] = $c['user_id'] == $_REQUEST['user_id'] ? 1 : 0;
@@ -81,8 +108,9 @@
             
             $user['user_city'] = $city;
             $ret['status'] = 'success';
-    		$ret['data']['user'] = $user;
+            $ret['data']['user'] = $user;
             $ret['data']['is_following'] = $is_following;
+            $ret['data']['buttonTxt'] = $buttonTxt;
             $ret['data']['followers'] = $followers;
             $ret['data']['followings'] = $followings;
             $ret['data']['pins'] = $pins;
